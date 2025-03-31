157f4fa1437d9564f446c928a2ff6fcafd855b4e
Gabriel Rodrigues <gabrielrvs104@gmail.com>
2025-03-31 09:57:52 -0300

    Removendo db.sqlite3 do versionamento

--- fluxo/templatetags/custom_filters.py
+++ fluxo/templatetags/custom_filters.py
@@ -10,20 +10,25 @@ def get_item(dictionary, key):
 
 @register.filter
 def calcular_preco(produto):
-    codigo_produto = produto.codigo_produto
-
-    produto_omie = buscar_produto_omie_detalhes(codigo_produto)
-    tabiela = produto_omie.get('produto_servico_cadastro')[0].get('tabelas_preco')
-    if tabiela:
-        codigo_Tabela_preco = tabiela[0].get('nCodTabPreco')
-        info_tabela = buscar_infos_tabela_omie(codigo_Tabela_preco)
-        prosentagem = info_tabela.get('outrasInfo').get('nPercAcrescimo')
-        prosentagem_abrev = (prosentagem / 100) + 1
-        custo_unitario = produto.custo_unitario
-        if custo_unitario == 0:
+    try:
+        codigo_produto = produto.codigo_produto
+        produto_omie = buscar_produto_omie_detalhes(codigo_produto)
+        print("produto_omie", produto_omie)
+        if not produto_omie:
             return 0
         else:
-            preco_calculado = float(custo_unitario) * prosentagem_abrev
-            return round(preco_calculado, 2)
-
-    return 0
+            tabiela = produto_omie.get('produto_servico_cadastro')[0].get('tabelas_preco')
+            if tabiela:
+                codigo_Tabela_preco = tabiela[0].get('nCodTabPreco')
+                info_tabela = buscar_infos_tabela_omie(codigo_Tabela_preco)
+                prosentagem = info_tabela.get('outrasInfo').get('nPercAcrescimo')
+                prosentagem_abrev = (prosentagem / 100) + 1
+                custo_unitario = produto.custo_unitario
+                if custo_unitario == 0:
+                    return 0
+                else:
+                    preco_calculado = float(custo_unitario) * prosentagem_abrev
+                    return round(preco_calculado, 2)
+            return 0
+    except:
+        return 0
